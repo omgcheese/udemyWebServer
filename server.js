c2c9fb164a3b499ec5c97965e5194619d3b4de6a
Jinwook Lee <jinwook.lee365@gmail.com>
2015-11-16 17:06:09 -0800

    Initialize repo

---
+++ server.js
@@ -0,0 +1,30 @@
+var express = require('express');
+var app = express();
+var PORT = 3000;
+
+var middleware = {
+	requireAuthentication: function (req, res, next) {
+		console.log('private route hit!');
+		next();
+	},
+	logger: function (req,res,next){
+		//new Date().toString()
+		var date = new Date().toString();
+		console.log('request: ' + date + ' ' + req.method + ' ' + req.originalUrl);
+		next();
+	}
+};
+
+app.use(middleware.logger);
+
+app.get('/about', middleware.requireAuthentication, function (req, res){
+
+	res.send("This page is about us!");
+});
+
+app.use(express.static('/Users/jinwooklee/web-server/public/'));
+//console.log(__dirname);
+
+app.listen(PORT, function () {
+	console.log("Express server started! Port: " + PORT);
+});
